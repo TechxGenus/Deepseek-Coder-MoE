6ac261cad4006c053345cf1072ff3b0186edf65d
Hao Jiang <jianghao0728@mail.ustc.edu.cn>
2024-03-08 14:50:00 +0800

    upload convert from mixtral

---
+++ convert_from_mixtral.py
@@ -0,0 +1,50 @@
+import shutil
+import argparse
+
+from Deepseek.configuration_deepseek import DeepseekConfig
+from Deepseek.modeling_deepseek import DeepseekForCausalLM
+from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_path", type=str, help="Path to the input model")
+parser.add_argument("--output_path", type=str, help="Path to save the output model")
+args = parser.parse_args()
+
+def map_param_name(name):
+    name = name.replace("mlp", "block_sparse_moe")
+    name = name.replace("gate_proj", "w1")
+    name = name.replace("up_proj", "w3")
+    name = name.replace("down_proj", "w2")
+    return name
+
+tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+
+dense_config = AutoConfig.from_pretrained(args.model_path).to_dict()
+dense_config["model_type"] = "deepseek"
+dense_config["aux_loss_alpha"] = dense_config["router_aux_loss_coef"]
+dense_config["n_routed_experts"] = dense_config["num_local_experts"]
+dense_config["moe_intermediate_size"] = dense_config["intermediate_size"]
+dense_config["norm_topk_prob"] = True
+dense_config["auto_map"] = {
+    "AutoConfig": "configuration_deepseek.DeepseekConfig",
+    "AutoModel": "modeling_deepseek.DeepseekModel",
+    "AutoModelForCausalLM": "modeling_deepseek.DeepseekForCausalLM"
+}
+dense_model = AutoModelForCausalLM.from_pretrained(args.model_path)
+
+sparse_config = DeepseekConfig(**dict(dense_config))
+sparse_model = DeepseekForCausalLM(sparse_config)
+
+param_dict = dict(dense_model.named_parameters())
+for name, param in sparse_model.named_parameters():
+    if name in param_dict:
+        param.data.copy_(param_dict[name].data)
+    elif map_param_name(name) in param_dict:
+        param.data.copy_(param_dict[map_param_name(name)].data)
+    else:
+        continue
+
+tokenizer.save_pretrained(args.output_path)
+sparse_model.save_pretrained(args.output_path)
+shutil.copy("Deepseek/configuration_deepseek.py", args.output_path)
+shutil.copy("Deepseek/modeling_deepseek.py", args.output_path)
